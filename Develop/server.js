4a4ad6cbd2d89af3a808ead8971a2958f902cdae
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-12-11 17:47:34 -0500

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -19,6 +19,8 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budgetTracker',
   {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
   }
 );
